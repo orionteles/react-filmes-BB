821211abce99acc3ee9c99e01576703d2421a0dc
Orion Teles de Mesquita <orionteles@gmail.com>
2023-04-17 22:13:46 -0300

    Detalhes de Filmes

---
+++ pages/filmes/orion.js
@@ -0,0 +1,12 @@
+import Pagina from '@/components/Pagina'
+import React from 'react'
+
+const orion = () => {
+    return (
+        <Pagina>
+            <div>orion</div>
+        </Pagina>
+    )
+}
+
+export default orion
